3e13f180f45431304444a05e74b84d1d28dcf55b
Bhargav-Bhamare <bbhamare9e20@gmail.com>
2025-12-27 15:34:24 +0530

    Adding Landing Page Code

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+
+//IMP Middlewares
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+// app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.get("/",(req,res)=> {
+    res.render("landing.ejs");
+});
+
+
+app.listen(8080,()=>{
+    console.log("Listening to port Successfully!");
+});
