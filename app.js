d92cfe3c3161b3b8df076b3c425f6e66a0d51c4e
Bhargav-Bhamare <bbhamare9e20@gmail.com>
2025-12-29 17:06:14 +0530

    Added Login Code and other files from the testAuth branch

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const authRoutes = require("./routes/authRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 //IMP Middlewares
+app.engine("ejs",ejsMate);
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
-// app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
+
+
+const session = require("express-session");
+app.use(authRoutes);
+app.use(dashboardRoutes);
 
 
 //Establishing Connection
@@ -22,6 +30,19 @@ main()
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/NyaayDrishti");
 };
+
+app.use(
+  session({
+    name: "judicial-session",
+    secret: "hackathon-secret-key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 // 1 hour
+    }
+  })
+);
 
 
 app.get("/",(req,res)=> {
@@ -36,6 +57,9 @@ app.get("/judgeDashboard",(req,res) =>{
     res.render("judge/judgeDash.ejs");
 });
 
+app.get("/login",(req,res) =>{
+    res.render("login.ejs");
+});
 
 app.listen(8080,()=>{
     console.log("Listening to port Successfully!");
